404465b76aad0efccf6f8fe94793f69fc7fe8e46
thomas2392 <thomas.ribeiro2392@gmail.com>
2023-03-20 15:55:04 -0300

    Project created

---
+++ src/main/java/com/tribeiro/sajc/SajcApplication.java
@@ -0,0 +1,13 @@
+package com.tribeiro.sajc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SajcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SajcApplication.class, args);
+	}
+
+}
